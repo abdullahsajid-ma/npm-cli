6f130ad23a17be8a59fe5678f00a54ad0b682d48
Abdullah sajid <abdullahsajid521@gmail.com>
2022-10-02 15:20:59 +0500

    npm-cli

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log(`
+
+    Abdullah Sajid
+
+    Software Engineer
+    Frontend | Html | Css | javascript | ReactJs
+
+    Github  :  https://github.com/abdullahsajid-ma
+    Twitter :  https://twitter.com/aabdullahsajid
+    Linkedin:  https://www.linkedin.com/in/abdullah-sajid-aa3892202/
+
+`)
+
+
+
+
